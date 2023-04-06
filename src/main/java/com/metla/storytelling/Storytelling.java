7465fa15a8316f66368d669e543cb82202e787c9
askiphy <durovam06@gmail.com>
2023-04-06 20:54:40 +0700

    Error fixed

--- src/main/java/com/metla/storytelling/Storytelling.java
+++ src/main/java/com/metla/storytelling/Storytelling.java
@@ -19,7 +19,7 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("Storytelling")
+@Mod("storytelling")
 public class Storytelling {
 
     // Directly reference a log4j logger.
